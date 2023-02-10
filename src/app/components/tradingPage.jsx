d46aa56add80065e11682c78a9999749020b9497
mikkan-39 <egor.altsoft2@gmail.com>
2023-02-10 16:29:03 +0300

    TradingPage layout

--- src/app/components/tradingPage.jsx
+++ src/app/components/tradingPage.jsx
@@ -1,9 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function TradingPage() {
+  const data = [
+    {
+      id: "1",
+      title: "First Item",
+    },
+    {
+      id: "2",
+      title: "Second Item",
+    },
+    {
+      id: "3",
+      title: "Third Item",
+    },
+  ];
+  const Header = () => {
+    return (
+      <View style={styles.headerView}>
+        <Text style={styles.sectionLabel}>Популярное</Text>
+        <FlatList
+          style={styles.roller}
+          horizontal={true}
+          contentContainerStyle={styles.rollerContent}
+          data={data}
+          keyExtractor={(item) => item.id}
+          ListHeaderComponent={Header}
+          renderItem={(item) => <View style={styles.miniCoin} />}
+        />
+        <Text style={styles.sectionLabel}>Все криптовалюты</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Trading Page</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={Header}
+        renderItem={(item) => <View style={styles.card} />}
+      />
     </View>
   );
 }
@@ -11,8 +48,36 @@ export default function TradingPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#eee",
+    alignItems: "stretch",
     justifyContent: "center",
+  },
+  headerView: {
+    marginBottom: 15,
+  },
+  sectionLabel: {
+    fontSize: 30,
+    marginLeft: 20,
+    marginTop: 15,
+    fontFamily: "System",
+  },
+  roller: {
+    margin: 10,
+    height: 50,
+    borderRadius: 15,
+    backgroundColor: "#fff",
+  },
+  rollerContent: { alignItems: "stretch" },
+  card: {
+    height: 100,
+    marginBottom: 15,
+    borderRadius: 15,
+    backgroundColor: "#fff",
+  },
+  miniCoin: {
+    width: 75,
+    margin: 10,
+    backgroundColor: "#999",
+    borderRadius: 5,
   },
 });
