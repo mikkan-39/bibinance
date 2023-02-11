b04b729d47837c9c9b76dc0a4c4464596e8a97fa
mikkan-39 <egor.altsoft2@gmail.com>
2023-02-11 15:36:46 +0300

    Removed unused slices

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import tradingPageReducer from "./slices/tradingPageSlice";
-import tickerPageReducer from "./slices/tickerPageSlice";
 import { api } from "./api/api";
 
 export const store = configureStore({
   reducer: {
-    tradingPage: tradingPageReducer,
-    tickerPage: tickerPageReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
